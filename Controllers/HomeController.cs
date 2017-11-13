80d10866284176c0b972f159199b4d34b60c8cee
Liam Read <liam.read@students.plymouth.ac.uk>
2017-11-13 16:46:40 +0000

    Updates and learning binding

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Bootstrap.ViewModels;
 
 namespace Bootstrap.Controllers
 {
@@ -15,6 +16,17 @@ namespace Bootstrap.Controllers
 
         [HttpGet("contact")]
         public IActionResult Contact()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// This action is where the information that has been sent by the user is "worked on".
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
         {
             return View();
         }
